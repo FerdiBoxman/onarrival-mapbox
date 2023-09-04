f32e41b0774b4d1bc6847c64cdaf349231a9da71
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-09-04 09:57:29 +0200

    changed name

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,7 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.ts'];
+const ENTRY_POINTS = ['src/index-v1.0.ts'];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
