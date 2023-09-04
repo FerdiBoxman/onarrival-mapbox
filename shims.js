e035be86f0b5b4ade853432fbdb401136f70ee90
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-09-04 11:16:53 +0200

    d

---
+++ shims.js
@@ -0,0 +1,6 @@
+// shims.js
+
+// Empty shims for Node.js built-ins to avoid esbuild warnings
+global.process = global.process || {};
+global.Buffer = global.Buffer || {};
+global.process.env = global.process.env || {};
