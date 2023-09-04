310e4f448f487d1d03c55b9fde74cdcc96dd996e
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-09-04 11:28:18 +0200

    d

--- shims.js
+++ shims.js
@@ -1,13 +1,12 @@
 // shims.js
 
-// Empty shims for Node.js built-ins to avoid esbuild warnings
-global.process = global.process || {};
-global.Buffer = global.Buffer || {};
-global.process.env = global.process.env || {};
-
-// eslint-disable-next-line no-console
 console.log('Shims.js is running');
 
 if (typeof global === 'undefined') {
   window.global = window;
 }
+
+// Empty shims for Node.js built-ins to avoid esbuild warnings
+global.process = global.process || {};
+global.Buffer = global.Buffer || {};
+global.process.env = global.process.env || {};
