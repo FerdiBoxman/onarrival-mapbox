75dfcbf57236b3afabe509307b8805f1887d06ab
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-09-04 11:30:56 +0200

    d

--- shims.js
+++ shims.js
@@ -1,5 +1,27 @@
 // shims.js
 
+// Inside shims.js
+if (typeof window !== 'undefined') {
+  window.require = function (module) {
+    switch (module) {
+      case 'path':
+        return {
+          // Your implementation here
+        };
+      case 'os':
+        return {
+          // Your implementation here
+        };
+      case 'crypto':
+        return {
+          // Your implementation here
+        };
+      default:
+        throw new Error(`Dynamic require of "${module}" is not supported`);
+    }
+  };
+}
+
 console.log('Shims.js is running');
 
 if (typeof global === 'undefined') {
